c56afc80e49f686ca8e31f824369ed170848513c
Dami Williams <dwill019@gold.ac.uk>
2020-02-12 20:01:48 +0000

    Initial Commit

---
+++ BarcodesScannerTests/BarcodesScannerTests.swift
@@ -0,0 +1,34 @@
+//
+//  BarcodesScannerTests.swift
+//  BarcodesScannerTests
+//
+//  Created by Damilola Sultan Williams on 12/02/2020.
+//  Copyright © 2020 Damilola Sultan Williams. All rights reserved.
+//
+
+import XCTest
+@testable import BarcodesScanner
+
+class BarcodesScannerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
